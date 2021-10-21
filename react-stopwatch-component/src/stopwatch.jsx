f3ef7172bc148b4ba8a44a935e3573bb54ab42ad
yanganboada <yanganboada@gmail.com>
2021-10-21 21:36:31 +0000

    React Stopwatch Component | Yang An Boada

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,27 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BeakerIcon, ZapIcon } from '@primer/octicons-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
 
 class StopWatch extends React.Component {
   constructor(props){
     super(props)
     this.state={
-      pause: true
+      pause: true,
+      count: 0,
+      intervalId: null
     }
-    this.handleStop = this.handleStop.bind(this)
+    this.handleControl = this.handleControl.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
 
-  handleStop(){
-    console.log(this.state.pause)
+  handleControl(){
+    if (this.state.pause && this.state.count <= 60) {
+      const interval = setInterval(() => {
+        this.setState({count: this.state.count+1});
+      }, 1000);
+      this.setState({pause: !this.state.pause, intervalId: interval})
+    }
+    this.setState({ pause: !this.state.pause})
+    clearInterval(this.state.intervalId)
+
+  }
+
+  handleReset(){
+    this.setState({ count: 0, intervalId: null})
   }
 
   render(){
      return (
-       <div onClick={this.handleStop}>
-         <div className='watch'>
-           <p>0</p>
+       <div className='container'>
+         <div className='watch' onClick={this.handleReset}>
+           <p className='second'>{this.state.count}</p>
          </div>
-         <TriangleRightIcon size={24} />
+         <div>
+          <FontAwesomeIcon
+            icon={this.state.pause ? faPlay : faPause}
+            size="3x"
+            onClick={this.handleControl}
+            className="control" />
+         </div>
        </div>
      )
   }
